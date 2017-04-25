dc7cb73150209f2b6ebc50380084037ec69e6bbc
MostafaDarwish <mostafayasser25@gmail.com>
2017-04-25 19:16:33 +0200

    Profile & Edit Profile are uploaded again

--- EditProfileFront.php
+++ EditProfileFront.php
@@ -36,7 +36,6 @@
         </div>
     </nav>
     <div class ="main">
-<<<<<<< HEAD
         <div class="col-md-8">
             <div class="card">
                 <div class="content">
@@ -56,27 +55,6 @@
                             $result2=$row2->fetch_assoc();
                         }
                         echo '<form accept-charset="UTF-8" role="form" method="post"action="EditProfileBack.php" method="POST">
-=======
-     <div class="col-md-8">
-        <div class="card">
-            <div class="content">
-                <?php
-                if (!empty($_SESSION['user2']))//then employee
-                {
-                    $qry = 'SELECT * FROM employee WHERE EID='.$_SESSION['user2']['EID'];
-                $row= $conn->query($qry);
-                if ($row->num_rows>0)
-                    {
-                        $result=$row->fetch_assoc();
-                    }
-                $qry2='SELECT Name FROM department WHERE DNO='.$_SESSION['user2']['DNO'];
-                $row2 = $conn->query($qry2);
-                if ($row2->num_rows>0)
-                    {
-                        $result2=$row2->fetch_assoc();
-                    }
-                echo '<form accept-charset="UTF-8" role="form" method="post"action="EditProfileBack.php" method="POST">
->>>>>>> 122acdcaa5acc8b760ee80ab1a4bfb3c28c1ca55
                     <!-- Photo div -->
 
                     <div id="userImage">
@@ -161,7 +139,6 @@
                     <div class="clearfix"></div>
                     </div>
                     </form>';
-<<<<<<< HEAD
                     }
                     else//then manager or ceo
                     {
@@ -178,24 +155,6 @@
                             $result2=$row2->fetch_assoc();
                         }
                         echo '<form accept-charset="UTF-8" role="form" method="post"action="EditProfileBack.php" method="POST">
-=======
-                }
-                else//then manager or ceo
-                {
-                    $qry = 'SELECT * FROM manager WHERE MID='.$_SESSION['user']['MID'];
-                $row= $conn->query($qry);
-                if ($row->num_rows>0)
-                    {
-                        $result=$row->fetch_assoc();
-                    }
-                $qry2='SELECT Name FROM department WHERE DNO='.$_SESSION['user']['DNO'];
-                $row2 = $conn->query($qry2);
-                if ($row2->num_rows>0)
-                    {
-                        $result2=$row2->fetch_assoc();
-                    }
-                echo '<form accept-charset="UTF-8" role="form" method="post"action="EditProfileBack.php" method="POST">
->>>>>>> 122acdcaa5acc8b760ee80ab1a4bfb3c28c1ca55
                     <!-- Photo div -->
 
                     <div id="userImage">
@@ -280,13 +239,8 @@
                     <div class="clearfix"></div>
                     </div>
                     </form>';
-<<<<<<< HEAD
                     }
                     ?>
-=======
-                }
-                ?>
->>>>>>> 122acdcaa5acc8b760ee80ab1a4bfb3c28c1ca55
                 </div>
             </div>
         </div>
