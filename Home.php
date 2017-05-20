7e02c7a33982753b1b25e96ffa13edb1f48ff3fe
ReemGira <reemgira18@gmail.com>
2017-05-20 23:14:32 +0200

    Update Home.php

--- Home.php
+++ Home.php
@@ -72,7 +72,7 @@ require "Common.php";
                if($_SESSION['user']['type'] == 2){  ?>
            <li><a href="department.php">Department Achievement</a></li>
                             <?php } }?> 
-              <li><a href="EmpAch.php">Employees</a></li>
+              <li><a href="view_emp.php">Employees</a></li>
               <li><a href="DeptsAch.php">Departments</a></li>
               <li><a href="logout.php">Log out</a></li>
               <!--<li class="divider"></li>-->
